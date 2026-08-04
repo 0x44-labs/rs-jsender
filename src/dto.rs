dba9c14a0de161d46b62b11f208975ea7d11a88e
Jacob Haap <git@j3.cx>
2026-08-04 15:10:40 +0200

    resolve gaps against JSend spec

--- src/dto.rs
+++ src/dto.rs
@@ -1,56 +1,74 @@
 use serde::Serialize;
-use std::collections::HashMap;
+use serde_json::{Map, Value};
+
+#[derive(Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum JSendStatus {
+    Success,
+    Fail,
+    Error,
+}
 
 #[derive(Serialize)]
 pub struct JSendResponse<T> {
-    pub(crate) status: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub(crate) data: Option<JSendData<T>>,
+    pub(crate) status: JSendStatus,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub(crate) message: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) code: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) data: Option<JSendData<T>>,
 }
 
 #[derive(Serialize)]
 #[serde(untagged)]
 pub enum JSendData<T> {
     Data(Option<T>),
-    Errors(HashMap<String, String>),
+    Value(Value),
 }
+
 impl<T> JSendResponse<T>
 where
     T: Serialize,
 {
     pub fn success(data: Option<T>) -> Self {
         Self {
-            status: "success".to_string(),
+            status: JSendStatus::Success,
             data: Some(JSendData::Data(data)),
             message: None,
+            code: None,
         }
     }
 
-    pub fn fail<I, K, V>(errors: I) -> Self
+    pub fn fail<I, K, V>(data: I) -> Self
     where
         I: IntoIterator<Item = (K, V)>,
         K: Into<String>,
-        V: Into<String>,
+        V: Into<Value>,
     {
-        let errors_map: HashMap<String, String> = errors
+        let fail_data: Map<String, Value> = data
             .into_iter()
             .map(|(k, v)| (k.into(), v.into()))
             .collect();
 
         Self {
-            status: "fail".to_string(),
-            data: Some(JSendData::Errors(errors_map)),
+            status: JSendStatus::Fail,
+            data: Some(JSendData::Value(Value::Object(fail_data))),
             message: None,
+            code: None,
         }
     }
 
-    pub fn error(message: impl Into<String>) -> Self {
+    pub fn error(
+        message: impl Into<String>,
+        code: Option<usize>,
+        data: Option<Value>,
+    ) -> Self {
         Self {
-            status: "error".to_string(),
-            data: None,
+            status: JSendStatus::Error,
             message: Some(message.into()),
+            code,
+            data: data.map(JSendData::Value),
         }
     }
 }
